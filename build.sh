ec9cda56ba3ba6f3a3d92044c238e63055b00310
Anjosy AJ <anjosy1996@gmail.com>
2025-05-13 16:36:07 +0530

    Fix deployment settings

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ set -o errexit
 pip install -r requirements.txt
  
 python manage.py collectstatic --no-input
-python manage.py migrate 
+python manage.py migrate
+
+# Create superuser if not exists
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'your-password') if not User.objects.filter(username='admin').exists() else print('Admin exists')" | python manage.py shell 
