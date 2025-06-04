d6bc27f27b676e9e96424df5b302827efb818344
Anjosy AJ <anjosy1996@gmail.com>
2025-06-04 12:23:32 +0530

    version 0.0.9

--- build.sh
+++ build.sh
@@ -11,12 +11,6 @@ python manage.py collectstatic --no-input
 # Run migrations
 python manage.py migrate
 
-# Create sqlite directory if it doesn't exist
-mkdir -p /opt/render/project/src/var/data
-
-# Create a symbolic link to the persistent directory
-ln -sf /opt/render/project/src/var/data/db.sqlite3 /opt/render/project/src/db.sqlite3
-
 # Create superuser if not exists
 python manage.py shell << END
 from django.contrib.auth.models import User
@@ -24,5 +18,5 @@ from django.db import IntegrityError
 try:
     User.objects.create_superuser('admin', 'admin@example.com', '$ADMIN_PASSWORD')
 except IntegrityError:
-    pass
+    print('Superuser already exists')
 END 
