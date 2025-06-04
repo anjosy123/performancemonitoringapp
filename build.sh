c9d18182a23743273eed48ecd9f2152be5c6e5b5
Anjosy AJ <anjosy1996@gmail.com>
2025-06-04 11:59:52 +0530

    version 0.0.7

--- build.sh
+++ build.sh
@@ -2,20 +2,20 @@
 # exit on error
 set -o errexit
 
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run migrations
+python manage.py migrate
 
 # Create sqlite directory if it doesn't exist
 mkdir -p /opt/render/project/src/var/data
 
 # Create a symbolic link to the persistent directory
 ln -sf /opt/render/project/src/var/data/db.sqlite3 /opt/render/project/src/db.sqlite3
-
-# Run migrations
-python manage.py makemigrations
-python manage.py migrate --noinput
-
-# Collect static files
-python manage.py collectstatic --no-input
 
 # Create superuser if not exists
 python manage.py shell << END
